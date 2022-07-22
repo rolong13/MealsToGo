d05b6fcb85e2efa2f2f10ed4569345a9cd62242a
Roger Long <docrogerlong@gmail.com>
2022-07-22 09:34:04 -0500

    Themes

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
 
 import { RestaurantScreen } from "./src/features/restaurants/screens/reastaurants.screen";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
+
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/restaurants/components/reastaurant-info-card.component.js
+++ src/features/restaurants/components/reastaurant-info-card.component.js
@@ -3,16 +3,17 @@ import styled from "styled-components";
 import { Card } from "react-native-paper";
 
 const Title = styled.Text`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
