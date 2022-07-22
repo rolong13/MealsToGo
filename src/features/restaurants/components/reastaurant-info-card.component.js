3168494036c70bca1be695abffe9cf8edf9802a2
Roger Long <docrogerlong@gmail.com>
2022-07-22 14:02:20 -0500

    added rating stars

--- src/features/restaurants/components/reastaurant-info-card.component.js
+++ src/features/restaurants/components/reastaurant-info-card.component.js
@@ -1,10 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 const RestaurantCard = styled(Card)`
@@ -28,10 +48,22 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  //var index = 0;
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} key={Math.random()} />
+          ))}
+        </Rating>
+        <Address> {address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
