6dfb60bab312298e92072cbb3124907763922682
Roger Long <docrogerlong@gmail.com>
2022-07-21 20:02:54 -0500

    6-Restaurant Info Card added

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
 import { RestaurantScreen } from "./src/features/restaurants/screens/reastaurants.screen";
 
 export default function App() {
   return (
     <>
       <RestaurantScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
--- src/features/restaurants/screens/reastaurants.screen.js
+++ src/features/restaurants/screens/reastaurants.screen.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
 import { RestaurantInfo } from "../components/reastaurant-info.component";
 
 export const RestaurantScreen = () => (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-            <Searchbar />
-        </View>
-        <View style={styles.list}>
-            <RestaurantInfo />
-        </View>
-    </SafeAreaView>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfo />
+    </View>
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight,
-    },
-    search: {
-        padding: 16,
-    },
-    list: {
-        flex: 1,
-        padding: 16,
-        backgroundColor: "blue",
-    },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
 });
